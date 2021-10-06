e615854decc2819512db99c6b665709162936656
Deat <debora2013aguiartavares@gmail.com>
2021-10-05 22:03:45 -0300

    empresa model created

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
+const { request } = require("express");
 const express = require("express");
 
 //connect to database
 require('./config/db.config');
 
 const app = express();
+
+app.post("/cadastro/empresa", (request, response) => {
+  //criar uma nova empresa no DB
+  
+
+  //retornar json da empresa criada
+});
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Server listen on Port ${process.env.PORT}`)
---
+++ models/Empresa.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require('mongoose');
+
+const empresaSchema = new Schema({
+    empresa: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    senha: {type: String, required: true},
+    },
+    {
+    timestamps:true,
+    }
+);
