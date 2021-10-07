02014071141d1266fc3f9c3b28e7eeab769403e4
jordanavq <jordanavq@gmail.com>
2021-10-07 17:13:54 -0300

    add basic setting to app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const { request } = require("express");
 const express = require("express");
-
-//connect to database
-require('./config/db.config');
+const cors = require("cors");
 
 const app = express();
 
+//middlewares gerais
+app.use(express.json());
+app.use(cors());
+
+//connect to database
+require("./config/db.config");
+
 app.post("/cadastro/empresa", (request, response) => {
   //criar uma nova empresa no DB
-  
-
   //retornar json da empresa criada
 });
-
 
 app.listen(process.env.PORT, () =>
   console.log(`Server listen on Port ${process.env.PORT}`)
