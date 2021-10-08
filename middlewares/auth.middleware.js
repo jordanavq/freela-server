13bcf2daf20a7087e0fb254166217190a6f4a05a
Deat <debora2013aguiartavares@gmail.com>
2021-10-07 21:24:53 -0300

    middleware authentication maded

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const auth = (req, res, next) => {
+    const token = req.get("Autorization");
+    if (!token) {
+        res.status(401).json({message:"Request without token."});
+        return;
+    }
+    const tokenWithoutBearer = token.split(' ')[1];
+    try {
+        const decodedToken = jwt.verify(tokenWithoutBearer, process.env.SECRET_JWT);
+        req.user = {...decodedToken};
+        next();
+        
+    } catch (error) {
+        res.status(401).json({message:"Unauthorized"});
+    }
+};
+
+module.exports = auth;
