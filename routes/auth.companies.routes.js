5ea688a97f48f7e87cd8aff77a7445a1ed1b638a
jordanavq <jordanavq@gmail.com>
2021-10-09 09:37:25 -0300

    UserCompany signup created

--- routes/auth.companies.routes.js
+++ routes/auth.companies.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const bcrypt = require("bcryptjs");
 
 const UserCompany = require("../models/Company.model");
 
@@ -16,7 +17,20 @@ router.post("/empresa/cadastro", async (req, res) => {
     if (!!user) {
       throw new Error("email already exists");
     }
-  } catch (error) {}
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(senha, salt);
+    await UserCompany.create({
+      empresa,
+      email,
+      senha,
+    });
+
+    res.status(201).json({ message: `Created user: ${email}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error trying to create user", error: error.message });
+  }
 });
 
 //router.post("/empresa/cadastro", async )
