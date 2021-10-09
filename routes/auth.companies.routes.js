63324747e9d0b024f509156be9893056a5feb670
jordanavq <jordanavq@gmail.com>
2021-10-09 08:12:59 -0300

    verifications from UserCompany auth

--- routes/auth.companies.routes.js
+++ routes/auth.companies.routes.js
@@ -5,10 +5,22 @@ const UserCompany = require("../models/Company.model");
 const router = Router();
 
 //signup-cadastro
+router.post("/empresa/cadastro", async (req, res) => {
+  const { empresa, email, senha } = req.body;
+
+  try {
+    if (!empresa || !email || !senha) {
+      throw new Error("Missing empresa or email or senha");
+    }
+    const user = await UserCompany.findOne({ email });
+    if (!!user) {
+      throw new Error("email already exists");
+    }
+  } catch (error) {}
+});
 
 //router.post("/empresa/cadastro", async )
 
 module.exports = router;
-
 
 //criar rota de login e signup aqui
