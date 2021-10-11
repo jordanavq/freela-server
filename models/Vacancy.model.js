4bb3d2d48af6d3559b7a84df5e37c529f5fbb641
jordanavq <jordanavq@gmail.com>
2021-10-11 19:20:23 -0300

    update

--- models/Vacancy.model.js
+++ models/Vacancy.model.js
@@ -5,7 +5,7 @@ const vacancySchema = new Schema(
     data: { type: String, require: true },
     horario: { type: String, require: true },
     funcao: { type: String, require: true },
-    diaria: { type: Number, require: true },
+    diaria: { type: String, require: true },
     traje: { type: String, require: true },
     fornece_refeicao: { type: Boolean, require: true },
     endereco: { type: String, require: true },
