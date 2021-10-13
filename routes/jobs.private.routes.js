c71c0a0af3d24a2db4854a31855826612fa04281
jordanavq <jordanavq@gmail.com>
2021-10-13 19:46:31 -0300

    route apllications created

--- routes/jobs.private.routes.js
+++ routes/jobs.private.routes.js
@@ -43,11 +43,31 @@ router.delete("/deletar/:vagaId", async (req, res) => {
   }
 });
 
+//Criar nova candidatura
+
+router.post("/candidatura/:vagaId", async (req, res) => {
+  const { vagaId } = req.params;
+  try {
+    //projeção de objeto
+    // const job = await UserJob.findById(vagaId, { funcao:1, cidade:1, estado:1 , _id:0 });
+    const job = await UserJob.findById(vagaId);
+    job.candidatosId.push(req.user.id);
+    job.save();
+    res.status(200).json({ message: "Application sent" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error trying to sent a application", error });
+  }
+});
+
 //Buscar vagas que o candidato está inscrito
 router.get("/:candidatoId", async (req, res) => {
   const { candidatoId } = req.params;
   try {
-    const applications = await UserJob.findByIdandUpdate(candidatoId);
+    const applications = await UserJob.find({
+      candidatosId: { $in: candidatoId },
+    });
     res.status(200).json(applications);
   } catch (error) {
     res
@@ -57,6 +77,19 @@ router.get("/:candidatoId", async (req, res) => {
 });
 
 //Buscar dentro da vaga publicada quais candidatos estão inscritos
-//router.get("/empresaId");
+router.get("/banana/:empresaId", async (req, res) => {
+  const { empresaId } = req.params;
+  console.log(empresaId);
+  try {
+    const applications = await UserJob.find({
+      empresaId: empresaId,
+    });
+    res.status(200).json(applications);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error trying to get all applicantions", error });
+  }
+});
 
 module.exports = router;
