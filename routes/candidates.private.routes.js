5bab1952a8a9af5a33d02e25d920870c45be25ec
jordanavq <jordanavq@gmail.com>
2021-10-21 21:31:27 -0300

    update candidates private route

--- routes/candidates.private.routes.js
+++ routes/candidates.private.routes.js
@@ -43,9 +43,9 @@ router.delete("/deletar/:candidateId", async (req, res) => {
 //buscar um candidato especifico
 router.get("/:candidatoId", async (req, res) => {
   const { candidatoId } = req.params;
-
+  console.log("------", candidatoId);
   try {
-    const candidate = await Candidate.findById(candidatoId, { senha: 0 });
+    const candidate = await UserCandidate.findById(candidatoId, { senha: 0 });
     res.status(200).json(candidate);
   } catch (error) {
     res
